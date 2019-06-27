01d5f6ff69d24721c7763716c46cbc141778e8c2
ericafenyo <ericafenyo@yahoo.com>
2019-06-27 13:39:41 +0200

    create domain layer with api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import './App.css';
-import ConsumptionReport from './Components/ConsumptionReport/ConsumptionReport';
-import PrevisionalConsumption from './Components/PrevisionalConsumption/PrevisionalConsuption';
-import Quizz from './Components/Quizz/Quizz';
-import Tips from './Components/Tips/Tips';
-import CurrentConsumption from './CurrentConsumption/CurrentConsumption';
+import React from "react";
+import "./App.css";
+import ConsumptionReport from "./Components/ConsumptionReport/ConsumptionReport";
+import PrevisionalConsumption from "./Components/PrevisionalConsumption/PrevisionalConsuption";
+import Quizz from "./Components/Quizz/Quizz";
+import Tips from "./Components/Tips/Tips";
+import CurrentConsumption from "./CurrentConsumption/CurrentConsumption";
+import { getRealTimeData, getTips } from "./store";
+
+import Computer from "./Computer";
 
 function App() {
+  getRealTimeData(data => console.log(data));
+  getTips(data => console.log(data));
+
+  console.log(
+    Computer.computePercentageConsumed(11.817760236000648, 162.3747006409853)
+  );
+
   return (
     <div className="App">
       <div className="col-6">
